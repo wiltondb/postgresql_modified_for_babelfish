981ca58af307d91da734af6e2db6c4e4ec7649e0
alex <alex@staticlibs.net>
2023-05-15 10:57:31 +0100

    Use pgwin_deps in build scripts

--- src/tools/msvc/config_default.pl
+++ src/tools/msvc/config_default.pl
@@ -13,20 +13,20 @@ our $config = {
 	ldap      => 1,        # --with-ldap
 	extraver  => undef,    # --with-extra-version=<string>
 	gss       => undef,    # --with-gssapi=<path>
-	icu       => undef,    # --with-icu=<path>
-	lz4       => undef,    # --with-lz4=<path>
-	zstd      => undef,    # --with-zstd=<path>
+	icu       => "$ENV{PGWIN_DEPS_DIR}/icu",    # --with-icu=<path>
+	lz4       => "$ENV{PGWIN_DEPS_DIR}/lz4",    # --with-lz4=<path>
+	zstd      => "$ENV{PGWIN_DEPS_DIR}/zstd",    # --with-zstd=<path>
 	nls       => undef,    # --enable-nls=<path>
 	tap_tests => undef,    # --enable-tap-tests
 	tcl       => undef,    # --with-tcl=<path>
 	perl      => undef,    # --with-perl=<path>
 	python    => undef,    # --with-python=<path>
-	openssl   => undef,    # --with-ssl=openssl with <path>
-	uuid      => undef,    # --with-uuid=<path>
-	xml       => undef,    # --with-libxml=<path>
-	xslt      => undef,    # --with-libxslt=<path>
+	openssl   => "$ENV{PGWIN_DEPS_DIR}/openssl",    # --with-ssl=openssl with <path>
+	uuid      => "$ENV{PGWIN_DEPS_DIR}/uuid",    # --with-uuid=<path>
+	xml       => "$ENV{PGWIN_DEPS_DIR}/xml",    # --with-libxml=<path>
+	xslt      => "$ENV{PGWIN_DEPS_DIR}/xslt",    # --with-libxslt=<path>
 	iconv     => undef,    # (not in configure, path to iconv)
-	zlib      => undef     # --with-zlib=<path>
+	zlib      => "$ENV{PGWIN_DEPS_DIR}/zlib"     # --with-zlib=<path>
 };
 
 1;
