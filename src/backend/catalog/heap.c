a5ec43dbf46f762ad7a63828db1dcbf87165fa93
Tim Chang <timchang514@users.noreply.github.com>
2024-04-02 17:15:40 -0400

    Added missing checks for parent queryEnv in certain ENR functions (#321) (#339)
    * Added missing checks for parent queryEnv in certain ENR functions
    
    
    
    * Add flag to control parent queryEnv search
    
    * Revert get_ENR_withoid since oid can't be duplicates
    
    * True->false to avoid altering existing behavior
    
    * Revert artifact
    
    ---------
    
    Signed-off-by: Tim Chang <changti@amazon.com>

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -1219,7 +1219,7 @@ heap_create_with_catalog(const char *relname,
 	 * But allow same-name ENR as long as the it's not in the current query env.
 	 */
 	existing_relid = get_relname_relid(relname, relnamespace);
-	if (existing_relid != InvalidOid && (!is_enr || get_ENR(currentQueryEnv, relname)))
+	if (existing_relid != InvalidOid && (!is_enr || get_ENR(currentQueryEnv, relname, false)))
 		ereport(ERROR,
 				(errcode(ERRCODE_DUPLICATE_TABLE),
 				 errmsg("relation \"%s\" already exists", relname)));
@@ -1233,7 +1233,7 @@ heap_create_with_catalog(const char *relname,
 	old_type_oid = GetSysCacheOid2(TYPENAMENSP, Anum_pg_type_oid,
 								   CStringGetDatum(relname),
 								   ObjectIdGetDatum(relnamespace));
-	if (OidIsValid(old_type_oid) && (!is_enr || get_ENR(currentQueryEnv, relname)))
+	if (OidIsValid(old_type_oid) && (!is_enr || get_ENR(currentQueryEnv, relname, false)))
 	{
 		if (!moveArrayTypeName(old_type_oid, relname, relnamespace))
 			ereport(ERROR,
